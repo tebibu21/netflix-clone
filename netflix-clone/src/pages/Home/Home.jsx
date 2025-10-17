640acff0dd1bee32fb33015231bb62afea1d033c
tebibu21 <tebibu211@gmail.com>
2025-10-17 11:43:39 +0300

    header and footer

---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../Header/Header'
+import Footer from '../../Footer/Footer'
+
+function Home() {
+  return (
+    <>
+      <Header /> 
+      <Footer />   
+    </>
+  )
+}
+
+export default Home
+
